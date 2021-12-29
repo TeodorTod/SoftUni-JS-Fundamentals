7f7f3231be59474d2a2ad3ca8763df047fe4c539
Teodor Todorov <96665339+TeodorTod@users.noreply.github.com>
2021-12-29 20:37:55 +0200

    Delete 11. Arrays directory

--- 11. Arrays/1. sumFirstAndLastArrayElements.js
+++
@@ -1,7 +0,0 @@
-function sumFirstAndLastArrayElements(input) {
-    let first = Number(input[0]);
-    let last = Number(input[input.length - 1]);
-    let sum = first + last;
-    console.log(sum);
-}
-sumFirstAndLastArrayElements([20, 30, 40]);
--- 11. Arrays/4. reverseAnArrayOfStrings.js
+++
@@ -1,5 +0,0 @@
-function reverseAnArrayOfStrings(arr) {
-    let newArr = arr.reverse();
-    console.log(newArr.join(" "));
-}
-reverseAnArrayOfStrings(['a', 'b', 'c', 'd', 'e']);
